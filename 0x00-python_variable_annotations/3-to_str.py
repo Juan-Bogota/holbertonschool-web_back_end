bb284812cdc1938eab5f74f9f1ec22fe938c0aac
Juan <1213@holbertonschool.com>
2020-10-19 18:57:34 -0500

    Task 3 >> 3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 3 >> 3. Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """ Write a type-annotated function floor which takes
+    a float n as argument and returns the floor of the float.
+    """
+    return str(n)
