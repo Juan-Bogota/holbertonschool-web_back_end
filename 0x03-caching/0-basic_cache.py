c3da9da487a82fe932228db6bc0084026842109d
Juan <1213@holbertonschool.com>
2020-10-26 15:51:42 -0500

    Task 0 >> Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Task 0 >> Basic dictionary 
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic Cache Class"""
+
+    def __init__(self):
+        """Constructor Method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put Method"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get Method"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
