21a40799b78a440e41663dcb70498b1c87924e7c
Juan <1213@holbertonschool.com>
2020-11-01 19:58:32 -0500

    filtered logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,8 +5,13 @@ Filtered Logger
 from typing import List
 import re
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ filter datum """
-    
-    message = re.sub('password=[a-z0-9A-Z]*' + separator,'password=' + redaction + separator , message)
-    return re.sub('date_of_birth=([0-9]*\D[0-9]*\D[0-9]*)' + separator, 'date_of_birth=' + redaction + separator, message )
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """filter datum"""
+
+    message = re.sub('password=.*?' + separator, 'password=' +
+                     redaction + separator, message)
+    return re.sub('date_of_birth=([0-9]{2}\\D[0-9]{2}\\D[0-9]{4})' +
+                  separator, 'date_of_birth=' + redaction + separator,
+                  message)
