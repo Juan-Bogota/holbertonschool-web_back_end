cbbefe1e3017d04e7f7c459f7e8024c6f13f9738
Juan <1213@holbertonschool.com>
2020-11-02 16:48:10 -0500

    Encript Pass Valid

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -10,3 +10,11 @@ def hash_password(password: str) -> bytes:
     pswd = bytes(password, encoding="utf-8")
     hashed = bcrypt.hashpw(pswd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Is Valid"""
+    if bcrypt.checkpw(password.encode('utf8'), hashed_password):
+        return True
+    else:
+        return False
