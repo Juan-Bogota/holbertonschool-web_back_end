95fb57541a465478db4a5a277d040786c46d73e9
Juan <1213@holbertonschool.com>
2020-11-01 20:23:55 -0500

    filtered logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -12,5 +12,5 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
     for field in fields:
         message = re.sub(field + '=.*?' + separator, field + '=' +
-                     redaction + separator, message)
+                         redaction + separator, message)
     return message
