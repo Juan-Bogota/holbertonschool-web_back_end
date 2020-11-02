32c10c07d66db5687b17f7bdbbdb3eb182358f5f
Juan <1213@holbertonschool.com>
2020-11-02 12:55:59 -0500

    Add Method get logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ from typing import List
 import re
 import logging
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
 class RedactingFormatter(logging.Formatter):
