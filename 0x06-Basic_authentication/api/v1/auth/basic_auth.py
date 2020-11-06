270dd842d5584fec4478b89ded57348a14a27a3d
Juan <1213@holbertonschool.com>
2020-11-06 16:01:39 -0500

    [feature] decoded

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module: Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Method: Decode Base64 Authorization"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header.encode('utf-8'))
+        except:
+            return None
+        return decoded.decode('utf-8')
