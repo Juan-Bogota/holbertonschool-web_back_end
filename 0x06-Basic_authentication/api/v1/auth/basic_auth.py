5c01950507372978fa64e3a3c00cc5aafe8be11a
Juan <1213@holbertonschool.com>
2020-11-06 17:57:22 -0500

    Basic - User object mandatory

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Decode Base64 Authorization
 """
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,19 @@ class BasicAuth(Auth):
             data = decoded_base64_authorization_header.split(':')
             return data[0], data[1]
         return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Method: Basic - User object"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            list_search = User.search({'email': user_email})
+            for item in list_search:
+                if item.is_valid_password(user_pwd):
+                    return item
+            return None
+        except Exception:
+            return None
