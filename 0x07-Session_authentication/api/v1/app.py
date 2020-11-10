eeadcaa6060dbe9ae34c3ff523f41340825aad34
Juan <1213@holbertonschool.com>
2020-11-10 15:55:18 -0500

    Session Empty

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -22,6 +22,9 @@ if AUTH_TYPE == 'auth':
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -55,7 +58,7 @@ def before_request():
         if auth.authorization_header(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
-        if request.current_user is None:
+        if auth.current_user(request) is None:
             abort(403)
 
 
