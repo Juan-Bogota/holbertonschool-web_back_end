8c8de9af801471c87f312610f80c3148a7575167
Juan <1213@holbertonschool.com>
2020-11-11 15:41:57 -0500

    Session LOgin

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Module of Session Auth views
+"""
+from flask import jsonify, abort, request, session, make_response
+from api.v1.views import app_views
+from models.user import User
+from os import getenv
+import json
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def sessionLogin() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - the status of the API
+    """
+    email = request.form.get('email')
+    if not email or email == '':
+        return jsonify({"error": "email missing"}), 400
+    password = request.form.get('password')
+    if not password or password == '':
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({'email': email})
+    if len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    for item in users:
+        if item.is_valid_password(password):
+            from api.v1.app import auth
+            resp = jsonify(item.to_json())
+            SESSION_NAME = getenv('SESSION_NAME')
+            resp.set_cookie(SESSION_NAME, auth.create_session(item.id))
+            return resp
+    return jsonify({"error": "wrong password"}), 404
