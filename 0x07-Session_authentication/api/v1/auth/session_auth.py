8fdf8fc30f695fc951a5f206c93e72a7e4f9afa6
Juan <1213@holbertonschool.com>
2020-11-10 19:01:19 -0500

    Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,18 @@ Module: Session Auth
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ CLass: Session Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Method:creates a Session ID"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
