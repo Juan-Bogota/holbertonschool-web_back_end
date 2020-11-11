3127956befb0bef31d91f6e7b513fbb3c25aae33
Juan <1213@holbertonschool.com>
2020-11-11 18:20:16 -0500

    Session LOgin

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -30,4 +30,5 @@ def sessionLogin() -> str:
             SESSION_NAME = getenv('SESSION_NAME')
             resp.set_cookie(SESSION_NAME, auth.create_session(item.id))
             return resp
-    return jsonify({"error": "wrong password"}), 404
+        else:
+            return jsonify({"error": "wrong password"}), 404
