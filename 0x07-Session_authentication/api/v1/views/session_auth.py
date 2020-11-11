32188c34b9d3d00bdfc04f59347fe7afdee8d515
Juan <1213@holbertonschool.com>
2020-11-11 15:56:33 -0500

    Session LOgin

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -22,15 +22,15 @@ def sessionLogin() -> str:
         return jsonify({"error": "password missing"}), 400
     try:
         users = User.search({'email': email})
-    except:
-        return jsonify({"error": "no user found for this email"}), 404
-    if len(users) == 0:
-        return jsonify({"error": "no user found for this email"}), 404
-    for item in users:
-        if item.is_valid_password(password):
-            from api.v1.app import auth
-            resp = jsonify(item.to_json())
-            SESSION_NAME = getenv('SESSION_NAME')
-            resp.set_cookie(SESSION_NAME, auth.create_session(item.id))
-            return resp
+        if len(users) == 0:
+            return jsonify({"error": "no user found for this email"}), 404
+        for item in users:
+            if item.is_valid_password(password):
+                from api.v1.app import auth
+                resp = jsonify(item.to_json())
+                SESSION_NAME = getenv('SESSION_NAME')
+                resp.set_cookie(SESSION_NAME, auth.create_session(item.id))
+                return resp
+    except Exception:
+        return None
     return jsonify({"error": "wrong password"}), 404
