6e9a8b1cdcdc1dfeb2fe4e50545b520f44da5321
Juan <1213@holbertonschool.com>
2020-11-16 23:17:06 -0500

    Task 2 - Find a user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -36,10 +36,11 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """This method takes in arbitrary keyword arguments and returns the first
         row found in the table as filtered by the method's input arguments."""
+        if not kwargs:
+            raise InvalidRequestError
 
-        
         c_names = User.__table__.columns._data.keys()
-        
+
         for key in kwargs.keys():
             if key in c_names:
                 ed_user = self._session.query(User).filter_by(**kwargs).first()
