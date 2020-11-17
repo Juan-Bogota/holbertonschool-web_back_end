6923b95965400ce921e173f5c0948ba5b493ebad
Juan <1213@holbertonschool.com>
2020-11-16 21:43:20 -0500

    Task 1 - create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module: Database"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from user import Base, User
+from typing import TypeVar
+
+
+class DB:
+    """Class: Database"""
+
+    def __init__(self):
+        """Method: Constructor"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Method: Creating a Session"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """Method: The method should save the user to the database."""
+        ed_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(ed_user)
+        self._session.commit()
+        return ed_user
