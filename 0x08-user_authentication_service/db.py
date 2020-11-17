5289317c53f3522409e78ff6cccf5f7645f79d99
Juan <1213@holbertonschool.com>
2020-11-16 21:51:50 -0500

    Task 1 - create user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base, User
-from typing import TypeVar
 
 
 class DB:
