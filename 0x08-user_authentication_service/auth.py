9d95fba6d0f4d61efd3eee481e38847db1578f69
Juan <1213@holbertonschool.com>
2020-11-17 17:39:53 -0500

    Hash password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Module: Authentication"""
+
 import bcrypt
 
 def _hash_password(password: str) -> str:
-    """Method: Return a string hash"""
+    """Method: Return a string hash of the input password"""
     passwd = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(passwd, salt)
