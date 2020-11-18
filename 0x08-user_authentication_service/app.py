f6bbb0966f99c8fc207de5ba67e79907d27bc208
Juan <1213@holbertonschool.com>
2020-11-17 22:09:34 -0500

    LOGIN User

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module: Basic Flask App"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -25,6 +25,20 @@ def users():
         return jsonify(dt)
     except ValueError:
         return jsonify({'message': 'email already registered'}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def sessions():
+    """Function: LOGIN a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    dt = {"email": email, "message": "logged in"}
+    resp = jsonify(dt)
+    session_id = AUTH.create_session(email)
+    resp.set_cookie('session_id', session_id)
+    return resp
 
 
 if __name__ == "__main__":
