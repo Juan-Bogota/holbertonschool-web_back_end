7cf1e80f394834801ab0200f2c8ce9e62f60080a
Juan <1213@holbertonschool.com>
2020-11-17 21:00:36 -0500

    Generate UUIDs

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,14 +6,20 @@ from db import DB
 from user import User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> str:
-    """Method: Return a string hash of the input password"""
+    """Function: Return a string hash of the input password"""
     passwd = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(passwd, salt)
     return hashed
+
+
+def _generate_uuid() -> str:
+    """Function: Generate UUIDs"""
+    return str(uuid.uuid4())
 
 
 class Auth:
