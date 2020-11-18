3a33793c59677df4ba61cf6a2a098f0ba5b50cce
Juan <1213@holbertonschool.com>
2020-11-17 19:36:51 -0500

    Basic Flask app post

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Module: Basic Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -8,6 +12,20 @@ app = Flask(__name__)
 def home():
     """Function: set up a basic Flask app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Function: End Point /users to register a user"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        new_user = AUTH.register_user(email, password)
+        dt = {"email": "{}".format(new_user.email), "message": "user created"}
+        return jsonify(dt), 400
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
