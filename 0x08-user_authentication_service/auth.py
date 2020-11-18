d910d8074797fb6d9b885fc5a7026d1ac489258d
Juan <1213@holbertonschool.com>
2020-11-18 18:13:00 -0500

    Update Password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -84,3 +84,13 @@ class Auth:
         new_uuid = _generate_uuid()
         user.reset_token = new_uuid
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Method: Update Password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed = _hash_password(password)
+        user.hashed_password = hashed
+        user.reset_token = None
