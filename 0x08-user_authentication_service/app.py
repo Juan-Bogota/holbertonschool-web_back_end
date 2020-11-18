88522d2903d14788be61162c853ba57dba49305c
Juan <1213@holbertonschool.com>
2020-11-18 18:05:59 -0500

    Get password Token 1

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -68,7 +68,10 @@ def get_reset_password_token():
     email = request.form.get('email')
     if email is None:
         abort(403)
-    token = AUTH.get_reset_password_token(email)
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
     return jsonify({"email": email, "reset_token": token}), 200
 
 
