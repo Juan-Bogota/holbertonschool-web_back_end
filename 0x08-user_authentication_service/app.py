1a58a477f7e3110eaba314d14245a74f6712c7d8
Juan <1213@holbertonschool.com>
2020-11-18 18:19:57 -0500

    Update Password APp

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -74,6 +74,18 @@ def get_reset_password_token():
         abort(403)
     return jsonify({"email": email, "reset_token": token}), 200
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """Update Password"""
+    email = request.form.get('email')
+    new_password = request.form.get('new_password')
+    reset_token = request.form.get('reset_token')
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
