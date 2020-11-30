688baf19f1e74977ed85aa4740a74af3840effb3
Juan <1213@holbertonschool.com>
2020-11-30 15:57:08 -0500

    Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module: Basic Flask App"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from os import getenv
+
+
+app = Flask(__name__)
+babel = Babel(app)
+config = Config()
+app.config['BABEL_DEFAULT_LOCALE'] = config.get_languages()
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+class Config:
+    """Class: Configure available languages in our app"""
+    self.LANGUAGES = ["en", "fr"]
+
+    def get_languages():
+        return self.LANGUAGES
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home_page():
+    """Function: Home Page 0-index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
