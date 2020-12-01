1e604ef2ea44654ac3359993b487019bdb7309ce
Juan <1213@holbertonschool.com>
2020-12-01 18:49:31 -0500

    Redis Basic

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,16 +3,17 @@
 
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
     def __init__(self):
-        """Method: Constructor"""
+        """Method: Constructor Cache"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method: should generate a random key"""
         key = str(uuid.uuid4())
-        self._redis.set(data, key)
+        self._redis.set(key, data)
         return key
