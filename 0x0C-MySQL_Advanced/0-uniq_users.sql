a8d74353c0c275ed9028d828fd5770cdbdc855f6
Juan <1213@holbertonschool.com>
2020-12-03 20:14:10 -0500

    Task > 0 - We are all unique

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- MySQL: script that creates a table users
--- Create a DB and table
+-- Create a DB and table users
 CREATE TABLE IF NOT EXISTS users (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 email VARCHAR(255)  NOT NULL UNIQUE,
---
+++ 0x0C-MySQL_Advanced/README.md
@@ -0,0 +1,2 @@
+# 0x0C. MySQL advanced
+> ## Specializations - Web Stack programming ― Back-end
