495fcfdbf485aebf3e6f7749460026023773c8ae
Juan <1213@holbertonschool.com>
2020-12-04 22:11:34 -0500

    Task 7- stored procedure

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,11 @@
 -- Triggers - MYSQL
--- Write a SQL script that creates a stored procedure for a student.
+-- Write a SQL script that creates a stored procedure that adds a new correction for a student.
 DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
 DECLARE prom INT;
-SET prom = (select AVG(score) from corrections GROUP BY user_id HAVING user_id=user_id);
-UPDATE users SET average_score=prom WHERE id=user_id;
+SET prom = (select AVG(score) from corrections AS newTable GROUP BY user_id HAVING newTable.user_id=user_id;);
+UPDATE users SET average_score=prom WHERE id=user_id ;
 
 END;
 |
