b19d780b3121fd4decfabdeb8cd3f0229c6742e0
Juan <1213@holbertonschool.com>
2020-12-04 22:12:51 -0500

    Task 7- stored procedure

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@ DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
 DECLARE prom INT;
-SET prom = (select AVG(score) from corrections AS newTable GROUP BY user_id HAVING newTable.user_id=user_id;);
+SET prom = (select AVG(score) from corrections AS newTable WHERE newTable.user_id=user_id;);
 UPDATE users SET average_score=prom WHERE id=user_id ;
 
 END;
