2ccfe315c15ca947ee477ca7f1c4c16413b7d5c4
Juan <1213@holbertonschool.com>
2020-12-04 20:25:12 -0500

    Task 5- triggers II

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,3 +1,5 @@
+-- Triggers - MYSQL
+-- script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 DELIMITER |
 CREATE TRIGGER StatusChangeMailTrigger
     BEFORE UPDATE ON users FOR EACH ROW
