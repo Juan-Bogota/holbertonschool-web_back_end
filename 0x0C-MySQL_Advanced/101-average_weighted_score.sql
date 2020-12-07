c181156e50d2021a70d9b8d6a17e43e063edfd51
Juan <1213@holbertonschool.com>
2020-12-07 17:20:02 -0500

    Average Weighted Score ALl

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- SQL script that creates stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and store the average weighted score for all students
+DROP PROCEDURE ComputeAverageWeightedScoreForUsers;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+  UPDATE users, 
+    (SELECT newTable.id, (SUM(score * weight)/SUM(weight)) AS average 
+    FROM users AS newTable 
+    INNER JOIN corrections ON newTable.id = corrections.user_id 
+    INNER JOIN projects ON corrections.project_id = projects.id
+    GROUP BY newTable.id)
+  AS query
+  SET users.average_score = query.average
+  WHERE users.id=query.id;
+END;
+$$
