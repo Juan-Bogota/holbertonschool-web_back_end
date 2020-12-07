29bc1dcf3dc9617b289242b2ec1ea19775e0d983
Juan <1213@holbertonschool.com>
2020-12-07 17:22:00 -0500

    Average Weighted Score

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     
 DECLARE average FLOAT;
-SET average = (SELECT (SUM(score * weight)/SUM(weight)) FROM corrections AS newTable INNER JOIN projects WHERE project_id = id AND newTable.user_id = user_id);
+SET average = (SELECT (SUM(score * weight)/SUM(weight)) FROM corrections AS newTable INNER JOIN projects ON project_id = id WHERE newTable.user_id = user_id);
 UPDATE users SET average_score=average WHERE id=user_id;
 
 END;
