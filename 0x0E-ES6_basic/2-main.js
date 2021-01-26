45e9cd34637c0abf482062905c2c282879136655
Juan <1213@holbertonschool.com>
2021-01-26 17:17:50 -0500

    [feature] Arrow Funtion

---
+++ 0x0E-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow';
+/*eslint-disable */
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
