402ecbc4f08fc3519e1ddd39f132c6248911cf84
Juan <1213@holbertonschool.com>
2021-05-17 12:44:10 -0500

    then and catch

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then((success) => {
-      console.log("Got a response from the API");
+      console.log('Got a response from the API');
       return { status: 200, body: success };
     })
-    .catch((err) => {
-      console.log("Got a response from the API");
-      new Error();
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
     });
 }
