026e07a3d6558bad2bb732afafdb36757e19eea9
Juan <1213@holbertonschool.com>
2021-05-17 21:41:39 -0500

    Combine

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,23 +1,22 @@
 export default function updateStudentGradeByCity(
   getListStudents,
   city,
-  newGrades
+  newGrades,
 ) {
-  if (typeof city !== "string") throw new Error("City must be an string");
+  if (typeof city !== 'string') throw new Error('City must be an string');
   if (
-    !Array.isArray(newGrades) &&
-    newGrades.every((el) => typeof el !== "object")
-  )
-    throw new Error("newGrades is not an Array of Objects");
+    !Array.isArray(newGrades)
+    && newGrades.every((el) => typeof el !== 'object')
+  ) throw new Error('newGrades is not an Array of Objects');
   if (Array.isArray(getListStudents)) {
     const filterStudent = getListStudents.filter((el) => el.location === city);
     return filterStudent.map((el) => {
       const [data] = newGrades.filter((item) => item.studentId === el.id);
       return {
         ...el,
-        grade: data ? data.grade : "N/A",
+        grade: data ? data.grade : 'N/A',
       };
     });
   }
-  throw new Error("getListStudents is not an Array");
+  throw new Error('getListStudents is not an Array');
 }
